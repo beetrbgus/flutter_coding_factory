cd2f8d1f0cb244087fe5a52cbc55bfbe14966e4b
beetrbgus <devbeetrb0214@gmail.com>
2024-06-28 16:53:36 +0900

    feat - 새 동영상 재생 기능 추가
    - 같은 영상이 아닐 때 위젯 갱신

--- vid_player/lib/component/custom_video_player.dart
+++ vid_player/lib/component/custom_video_player.dart
@@ -8,10 +8,12 @@ import 'package:video_player/video_player.dart';
 // 동영상 위젯 생성
 class CustomVideoPlayer extends StatefulWidget {
   final XFile video;
+  final GestureTapCallback onNewVideoPressed;
 
   const CustomVideoPlayer({
     super.key,
     required this.video,
+    required this.onNewVideoPressed,
   });
   @override
   State<StatefulWidget> createState() => _CustomVideoPlayerState();
@@ -20,11 +22,20 @@ class CustomVideoPlayer extends StatefulWidget {
 class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
   VideoPlayerController? videoPlayerController;
   final Duration skipTime = const Duration(seconds: 3);
+  bool showControlIcons = false;
 
   @override
   void initState() {
     super.initState();
     initializeVideoController();
+  }
+
+  @override
+  void didUpdateWidget(covariant CustomVideoPlayer oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.video.path != widget.video.path) {
+      initializeVideoController();
+    }
   }
 
   // dispose 안하면 앱 실행 될 때마다 찌꺼기가 남아있음.
@@ -90,55 +101,67 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
       );
     }
 
-    return AspectRatio(
-      aspectRatio: videoPlayerController!.value.aspectRatio,
-      child: Stack(
-        children: [
-          VideoPlayer(videoPlayerController!),
-          Positioned(
-            bottom: 0,
-            right: 0,
-            left: 0,
-            child: Slider(
-              min: 0,
-              value: videoPlayerController!.value.position.inSeconds.toDouble(),
-              max: videoPlayerController!.value.duration.inSeconds.toDouble(),
-              onChanged: (double val) {
-                videoPlayerController!.seekTo(
-                  Duration(seconds: val.toInt()),
-                );
-              },
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          showControlIcons = !showControlIcons;
+        });
+      },
+      child: AspectRatio(
+        aspectRatio: videoPlayerController!.value.aspectRatio,
+        child: Stack(
+          children: [
+            VideoPlayer(videoPlayerController!),
+            if (showControlIcons)
+              Positioned(
+                bottom: 0,
+                right: 0,
+                left: 0,
+                child: Slider(
+                  min: 0,
+                  value: videoPlayerController!.value.position.inSeconds
+                      .toDouble(),
+                  max: videoPlayerController!.value.duration.inSeconds
+                      .toDouble(),
+                  onChanged: (double val) {
+                    videoPlayerController!.seekTo(
+                      Duration(seconds: val.toInt()),
+                    );
+                  },
+                ),
+              ),
+            Align(
+              alignment: Alignment.topRight,
+              child: CustomIconButton(
+                onPressed: widget.onNewVideoPressed,
+                iconData: Icons.photo_camera_back,
+              ),
             ),
-          ),
-          Align(
-            alignment: Alignment.topRight,
-            child: CustomIconButton(
-              onPressed: () {},
-              iconData: Icons.photo_camera_back,
-            ),
-          ),
-          Align(
-              alignment: Alignment.center,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  CustomIconButton(
-                    onPressed: onReversePressed,
-                    iconData: Icons.rotate_left,
-                  ),
-                  CustomIconButton(
-                    onPressed: onPlayPressed,
-                    iconData: videoPlayerController!.value.isPlaying
-                        ? Icons.pause
-                        : Icons.play_arrow_outlined,
-                  ),
-                  CustomIconButton(
-                    onPressed: onForwardPressed,
-                    iconData: Icons.rotate_right,
-                  ),
-                ],
-              )),
-        ],
+            if (showControlIcons)
+              Align(
+                alignment: Alignment.center,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    CustomIconButton(
+                      onPressed: onReversePressed,
+                      iconData: Icons.rotate_left,
+                    ),
+                    CustomIconButton(
+                      onPressed: onPlayPressed,
+                      iconData: videoPlayerController!.value.isPlaying
+                          ? Icons.pause
+                          : Icons.play_arrow_outlined,
+                    ),
+                    CustomIconButton(
+                      onPressed: onForwardPressed,
+                      iconData: Icons.rotate_right,
+                    ),
+                  ],
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
--- vid_player/lib/home_screen.dart
+++ vid_player/lib/home_screen.dart
@@ -59,6 +59,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Center(
       child: CustomVideoPlayer(
         video: video!,
+        onNewVideoPressed: onNewVideoPressed,
       ),
     );
   }
